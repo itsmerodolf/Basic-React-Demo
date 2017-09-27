886979b1f2b1a7ae308002d0d9c8291edb6ac433
itsmerodolf <DotDot_12000@yahoo.com>
2017-09-27 11:55:53 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Counter App
+this app uses React to do a simple demo of components, props and state.
+
+### Features
+
+- Welcome component
+- Today component
+- Counter Component
+
+### Usage
+```
+$ npm start
+```
+
+Visit to localhost:3000
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Welcome from './Welcome.js';
+import Today from './Today.js';
+import Counter from './Counter.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Welcome name='Rodolf' />
+        <Today day='Wednesday' />
+
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default App;
